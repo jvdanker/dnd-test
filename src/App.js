1e08325baf5892ceeab1520d8c03747000d79042
Juan van den Anker <juan@vdanker.net>
2018-10-28 21:42:12 +1300

    Sanitize data

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = data;
+        this.state = this.sanitize(data);
 
         this.onMove = this.onMove.bind(this);
         this.addComponent = this.addComponent.bind(this);
@@ -25,6 +25,33 @@ class App extends Component {
         this.updateModel = this.updateModel.bind(this);
 
         this.updateModel(true);
+    }
+
+    sanitize(data) {
+        var wires = data.wires.map(w => {
+            if (typeof w.value === 'undefined') {
+                w.value = false;
+            }
+
+            return w;
+        });
+        data.wires = wires;
+
+        var components = Object.keys(data.components).map(key => {
+            var c = data.components[key];
+
+            for (var i=0; i<c.connectors.length; i++) {
+                var conn = c.connectors[i];
+                if (typeof conn.value === 'undefined') {
+                    conn.value = false;
+                }
+            }
+
+            return c;
+        });
+        data.components = components;
+
+        return data;
     }
 
     onMove(e) {
@@ -136,23 +163,10 @@ class App extends Component {
             for (var j=0; j<wires.length; j++) {
                 var w = wires[j];
 
-                // debugger;
-                // console.log(i, w);
-
                 var from = components[w.from.component];
                 var fromC = from.connectors.find(c => c.id === w.from.port);
                 var to = components[w.to.component];
                 var toC = to.connectors.find(c => c.id === w.to.port);
-
-                if (typeof w.value === 'undefined') {
-                    w.value = false;
-                }
-                if (typeof fromC.value === 'undefined') {
-                    fromC.value = false;
-                }
-                if (typeof toC.value === 'undefined') {
-                    toC.value = false;
-                }
 
                 if (fromC.value !== w.value) {
                     changed = true;
@@ -172,16 +186,6 @@ class App extends Component {
                     var c1 = c.connectors[0];
                     var c2 = c.connectors[1];
                     var c3 = c.connectors[2];
-
-                    if (typeof c1.value === 'undefined') {
-                        c1.value = false;
-                    }
-                    if (typeof c2.value === 'undefined') {
-                        c2.value = false;
-                    }
-                    if (typeof c3.value === 'undefined') {
-                        c3.value = false;
-                    }
 
                     var result = !(c1.value && c2.value);
                     if (result !== c3.value) {
