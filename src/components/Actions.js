da92bac1f08e3a496e79e1fbd7780b95bed7322e
Juan van den Anker <juan@vdanker.net>
2018-11-11 22:03:17 +1300

    Refactoring

--- src/components/Actions.js
+++ src/components/Actions.js
@@ -1,13 +1,5 @@
 import React from 'react';
 
-import Wires from '../components/Wires';
-import Components from '../components/Components';
-import VisibleComponents from '../containers/VisibleComponents';
-import Node from './Node';
-import Switch from '../components/Switch';
-import ActionsComponent from "../containers/ActionsComponent";
-import RootComponent from "../containers/RootComponent";
-
 class Actions extends React.Component {
 
     render() {
